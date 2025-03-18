9ae8049a4c40a10e979535033e6cd01b0915705e
Lorenzo <lorenzo.galassi91@gmail.com>
2025-03-18 16:23:07 +0000

    fix: site title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'MystiWord - Find the hidden word',
+      htmlAttrs: {
+        lang: 'en',
+      },
+    }
+  },
+  
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
